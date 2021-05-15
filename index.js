b552f3f6b589cbfd56e9847fc0109f7a4b8f487e
emanuelbanega <62112962+emanuelbanega@users.noreply.github.com>
2021-05-14 23:47:21 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("./mongo.js");
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hola mundo");
+});
+
+app.use("/api/combis", require("./Routers/CombiRouter.js"));
+app.use("/api/choferes", require("./Routers/ChoferRouter.js"));
+app.use("/api/lugares", require("./Routers/LugarRouter.js"));
+app.use("/api/insumos", require("./Routers/InsumoRouter.js"));
+app.use("/api/pasajeros", require("./Routers/PasajeroRouter.js"));
+app.use("/api/rutas", require("./Routers/RutaRouter.js"));
+app.use("/api/viajes", require("./Routers/ViajeRouter.js"));
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log("Server on port " + PORT);
+});
